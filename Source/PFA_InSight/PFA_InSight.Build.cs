18966b0039992296625d05e5e21beabd07f598c1
Thibault CHANUT <t.chanut@game-sup.fr>
2026-06-03 22:51:49 +0200

    Build Possible (fix gas bug & finish interactible)

--- Source/PFA_InSight/PFA_InSight.Build.cs
+++ Source/PFA_InSight/PFA_InSight.Build.cs
@@ -10,7 +10,7 @@ public class PFA_InSight : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
